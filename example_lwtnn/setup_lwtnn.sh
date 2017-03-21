617ee3287eaaf1f851c93ab28dbd91a52256b7ba
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-03-21 16:38:13 +0100

    Do not clone the full repo every time

--- example_lwtnn/setup_lwtnn.sh
+++ example_lwtnn/setup_lwtnn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get eigen
-git clone https://github.com/RLovelett/eigen
+git clone https://github.com/RLovelett/eigen --depth 1
 cd eigen
 git checkout 3.3.0
 cd ..
@@ -10,9 +10,13 @@ cd ..
 export LWTNN_CXX_FLAGS="-I$PWD/eigen"
 
 # Get lwtnn
-git clone https://github.com/lwtnn/lwtnn
+git clone https://github.com/lwtnn/lwtnn --depth 1
 cd lwtnn
 git checkout v2.0
 
 # Build lwtnn
 make -j4
+cd ..
+
+# Tell your system where to find the liblwtnn.so library
+export LD_LIBRARY_PATH=lwtnn/lib
